0d3be42aa519a7bcd6e8c7f06fb5c81a2784178f
Harry Lascelles <harry@firstbanco.com>
2013-02-19 08:52:17 +0000

    Removing unnecessary loader

--- lib/session/check.rb
+++ lib/session/check.rb
@@ -4,9 +4,6 @@ module Session
   module Check
     class Engine < ::Rails::Engine
       initializer "session-check.loader" do
-        ActiveSupport.on_load(:action_controller) do
-          helper_method :session_check
-        end
       end
     end
   end
