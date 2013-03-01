2fa6299013cf8a59d7bd84872279bd1b9a9e8e19
Harry Lascelles <harry@firstbanco.com>
2013-03-01 14:13:31 +0000

    Preparing 0.1.5. Added many more options.

--- app/helpers/session_check_helper.rb
+++ app/helpers/session_check_helper.rb
@@ -2,8 +2,14 @@ require 'devise'
 
 module SessionCheckHelper
 
-  def session_check
-    render :partial => '/session_check', locals: {session_time: Devise.timeout_in}
+  def session_check(options)
+    locals = {
+      session_time: Devise.timeout_in,
+      check_every: 10,
+      reset_counter_on_ajax: true,
+      logged_out_url: '/users/sign_in'
+    }.merge options
+    render :partial => '/session_check', locals: locals
   end
 
 end
